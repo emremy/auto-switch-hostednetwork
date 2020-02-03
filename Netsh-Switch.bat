ead149dea28d49078adef638bc9e6225dd6a2067
Emre Mert <52324808+emremy@users.noreply.github.com>
2020-02-04 01:06:04 +0300

    Add files via upload

---
+++ Netsh-Switch.bat
@@ -0,0 +1,24 @@
+@echo off
+
+
+netsh wlan show hostednetwork|find "Status"|find "Started">nul && goto stop || goto start
+
+
+:start
+    
+	netsh wlan start hostednetwork
+
+	goto end
+
+
+:stop
+    
+	netsh wlan stop hostednetwork
+
+	goto end
+
+
+:end 
+    
+	timeout 2
+    exit
